7bd0a192c0a2fcde073343a344e3aafbc133b91e
DanielMaymon <36988885+DanielMaymon@users.noreply.github.com>
2023-11-30 12:25:39 +0200

    clerk prov

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,38 +1,81 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+  SignIn,
+  SignUp,
+  UserButton,
+} from "@clerk/clerk-react";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+
 import Home from './pages/HomePage/Home';
 import Login from './pages/LoginPage/Login';
 import Signup from './pages/SignupPage/Signup';
 import PlaceDeatile from './pages/PlaceDeatilePage/PlaceDeatile';
 import { element } from 'prop-types';
-import Root from './pages/HomePage/Root';
+import Root from "./pages/HomePage/Root";
 
-const router = createBrowserRouter([
-   {     
-   path:"/",
-   element:<Root/>,
-   id:'root',
-  children:[{ path:'/', element: <Home /> },
-  {
-    path: "login",
-    element: <Login/>,
-  },
-  {
-    path: "signup",
-    element: <Signup/>,
-  },
-  {
-    path: "asset/:id",
-    element: <PlaceDeatile/>,
-  },
-  // {
-  //   path: "products/:id",
-  //   element: <DeatilesPage />,
-  // },
-  ],
+/*
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
 }
-]);
+*/
+ 
+const clerkPubKey = "pk_test_c3RlYWR5LWNyYXdkYWQtNTkuY2xlcmsuYWNjb3VudHMuZGV2JA"
+
+console.log(clerkPubKey)
+ 
+
+
+
+function ClerkProviderWithRoutes() {
+
+  const navigate = useNavigate();
+ 
+  return (
+    <ClerkProvider
+      publishableKey={clerkPubKey}
+      navigate={(to) => navigate(to)}
+    >
+      <Routes>
+        <Route path="/" element={<Root />} children={
+          <>
+          <Route path="/" element={<Home/>} />
+          <Route path="/sign-up" element={<SignUp/>} />
+          </>
+        }
+        />
+        {/*
+        <Route
+          path="/sign-up/*"
+          element={<SignUp routing="path" path="/sign-up" />}
+        />
+        <Route
+          path="/protected"
+          element={
+          <>
+            <SignedIn>
+              
+            </SignedIn>
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
+          </>
+          }
+        />
+        */}
+      </Routes>
+    </ClerkProvider>
+  );
+}
+ 
 
 
 export function Router() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <ClerkProviderWithRoutes />
+    </BrowserRouter>
+  )
 }
