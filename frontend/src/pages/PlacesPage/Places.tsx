fbaa0f8db174b84a371154d1b7dd946688123909
hanzinisthebest <94485980+hanzinisthebest@users.noreply.github.com>
2023-12-07 16:08:55 +0200

    finished infinte scrolling without quiers

--- frontend/src/pages/PlacesPage/Places.tsx
+++ frontend/src/pages/PlacesPage/Places.tsx
@@ -1,50 +1,89 @@
 import React, { useEffect, useState } from 'react';
 import PlaceCard from './UI/PlaceCard';
-import { Button , Grid, LoadingOverlay, SimpleGrid } from '@mantine/core';
+import { Button , Grid, Loader, LoadingOverlay, SimpleGrid } from '@mantine/core';
 import Loading from '../Shared/Loading';
 import { useGetAssets } from '../../Querys/query-assets';
-import { useInfiniteQuery } from '@tanstack/react-query'
-import { fetchAssets } from '@/api/api-assets';
+// import { useInfiniteQuery } from '@tanstack/react-query'
+// import { fetchAssets } from '@/api/api-assets';
 import Asset from '@/models/assets';
+import axios, { AxiosResponse } from 'axios';
+import InfiniteScroll from 'react-infinite-scroller';
+import { useInfiniteQuery } from 'react-query';
 
 
 interface Props {
   
 }
 
-const Places: React.FC<Props> = () => {
-  const [showMore, setShowMore] = useState(false);
-  const [visible, setVisible] = useState(12); // Number of items to show initially
+// async function fetchAssets({ pageParam = 1 }): Promise<Asset[]> {
+//   const response: AxiosResponse<Asset[]> = await axios.get(`http://localhost:4000/api/assets?_page=${pageParam}&_limit=10`);
+//   return response.data;
+// }
 
-  const toggleShowMore = () => {
-    setShowMore(!showMore);
+const Places: React.FC<Props> = () => {
+
+
+  const [items, setItems] = useState<Asset[]>([]);
+  const [page, setPage] = useState<number>(1);
+
+  const [hasMore, setHasMore] = useState(true);
+
+  const fetchData = async (__page:number) => {
+    console.log(__page);
+    const response = await axios.get(
+      `http://localhost:4000/api/assets?_page=${page}&_limit=9`
+    );
+    setItems([...items, ...response.data]);
+    setPage(page + 1);
+
+    // Check if we've reached the end of the data
+  if (response.data.length < 9) {
+    setHasMore(false);
+  }
   };
-  const { isLoading, error, data } = useGetAssets();
-  if (isLoading) {
-    return <Loading />;
-  }
-  if(error){
-    <h1>error</h1>
-  }
-  if(!data){
-    return <h1>empty</h1>;
-  }
-  const loadMore = () => { 
-    setVisible(prevVisible => prevVisible + 12); // Show 10 more items
-  };
+
+  // const {
+  //   data,
+  //   fetchNextPage,
+  //   hasNextPage,
+  //   isLoading,
+  //   isError,
+  // } = useInfiniteQuery('assets', fetchAssets, {
+  //   getNextPageParam: (lastPage) => lastPage.nextPage,
+  // });
+
+  // if (isLoading) {
+  //   return <div>Loading...</div>;
+  // }
+
+  // if (isError) {
+  //   return <h1>Error</h1>;
+  // }
   return (
     <>
-     <SimpleGrid cols={3}>
-      {data.slice(0, visible).map((item)=>{
+        <InfiniteScroll
+      style={{ margin: "10px" }}
+      pageStart={0}
+      loadMore={fetchData}
+      hasMore={hasMore}
+      loader={
+        <Loader color="blue" />
+      }
+    >
+           <SimpleGrid cols={3}>
+      {items.map((item)=>{
         return(
           <PlaceCard id={item._id} available={item.isKitchen} name={item.city} pets={item.arePetsAllowed} freeParking = {item.isFreeParking} 
           grownupsNum={item.grownupsNum} childrenNum={item.childrenNum} babies={item.babies}  />
         )
       })}
     </SimpleGrid>
+      
+    </InfiniteScroll>
+
 
     {/* <LoadingOverlay visible={isLoading} /> */}
-    {!isLoading && <Button onClick={loadMore}>Show More</Button>}
+    {/* {!isLoading && <Button onClick={loadMore}>Show More</Button>} */}
      {/* <Button onClick={toggleShowMore}>
         {showMore ? 'Show Less' : 'Show More'}
       </Button> */}
